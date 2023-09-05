a8231d9994ef8bb795560dfdf7efa2e8e5971b93
Suah-Cho <sacho0929@gmail.com>
2023-09-05 17:34:22 +0900

    into listview

--- pybo.py
+++ pybo.py
@@ -3,7 +3,6 @@ import math
 from flask import Flask, render_template, request, redirect, url_for
 import pymysql
 
-
 # 데이터 베이스 연동
 db = pymysql.connect(host="localhost", 
                      user="root", password="passwd", 
@@ -16,29 +15,13 @@ cursor = db.cursor()
 app = Flask(__name__)
 
 @app.route('/', defaults={'page':1})
-# def list() :
-
-#     # cursor.execute("SELECT b.boardId, b.title, u.ID, b.location, date_format(b.createAt, '%Y-%m-%d') FROM board as b LEFT OUTER JOIN user as u on u.userId = b.userId WHERE b.status = 'active' ORDER BY b.createAt DESC;")
-#     # rows = cursor.fetchmany(size=10)
-
-#     # while rows :
-#     #     data_list = cursor.fetchmany(size=10)
-#     #     return render_template("list.html", data_list = data_list)
-
-    
-#     # print(rows)
-#     # data_list = cursor.fetchall()
-
-
-#     # page = request.args.get('page', type=int, default=1)
-#     # data_list = data_list.paginate(page, per_page=10)
-
-#     return render_template("list.html")
-@app.route('/<int:page>')
+@app.route('/list/<int:page>')
 def paging(page) :
+    print(page)
+
     perpage = 10
-    startat=page*perpage
-    cursor.execute("SELECT b.boardId, b.title, u.ID, b.location, date_format(b.createAt, '%Y-%m-%d') FROM board as b LEFT OUTER JOIN user as u on u.userId = b.userId WHERE b.status = 'active' ORDER BY b.createAt DESC LIMIT "+str(startat)+", "+str(perpage)+";")
+    startat=(page-1)*perpage
+    cursor.execute("SELECT b.boardId, b.title, u.ID, b.location, date_format(b.createAt, '%Y-%m-%d') FROM Board as b LEFT OUTER JOIN User as u on u.userId = b.userId WHERE b.status = 'active' ORDER BY b.createAt DESC LIMIT "+str(startat)+", "+str(perpage)+";")
     data_list = list(cursor.fetchall())
 
     return render_template("list.html", data_list = data_list)
@@ -47,16 +30,25 @@ def paging(page) :
 def edit() :
     return render_template("edit.html")
 
-@app.route('/view', methods=['GET', 'POST'])
+@app.route('/view')
 def view() :
     print(request.method)
     
-    cursor.execute("SELECT b.boardId, b.title, u.ID, b.content, b.location, date_format(b.createAt, '%Y-%m-%d') FROM board as b LEFT OUTER JOIN user as u on u.userId = b.userId WHERE boardId = (SELECT MAX(boardId) FROM board) ORDER BY b.createAt DESC;",)
+    cursor.execute("SELECT b.boardId, b.title, u.ID, b.content, b.location, date_format(b.createAt, '%Y-%m-%d') FROM Board as b LEFT OUTER JOIN User as u on u.userId = b.userId WHERE boardId = (SELECT MAX(boardId) FROM Board) ORDER BY b.createAt DESC;",)
     data = cursor.fetchall()
     print(data)
 
     return render_template("view.html", data = data)
-    
+
+@app.route('/listview/<int:id>')    
+def view2(id) :
+    print("id = ", id)
+    cursor.execute("SELECT b.boardId, b.title, u.ID, b.content, b.location, date_format(b.createAt, '%Y-%m-%d') FROM Board as b LEFT OUTER JOIN User as u on u.userId = b.userId WHERE boardId = {} ORDER BY b.createAt DESC;".format(id))
+    data = cursor.fetchall()
+    print(data)
+
+    return render_template("view.html", data = data)
+
 
 
 @app.route('/write/', methods=['GET', 'POST'])
@@ -72,11 +64,10 @@ def write() :
         contents = request.form['body']
        
         
-        cursor.execute("INSERT INTO board (userId, title, content, location) VALUES ((SELECT userId FROM user WHERE ID = %s), %s, %s, %s);", (user, title, contents, location))
+        cursor.execute("INSERT INTO Board (userId, title, content, location) VALUES ((SELECT userId FROM User WHERE ID = %s), %s, %s, %s);", (user, title, contents, location))
         cursor.connection.commit()
 
         return redirect(url_for('view'))
-    # return render_template("write.html")
 
 
 
